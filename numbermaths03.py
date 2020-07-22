ff73f1831355d53d452ef0895f6ca669b32fccdf
Anands-88 <anandsholla8@gmail.com>
2020-07-22 20:31:55 +0530

    added numbermath file

---
+++ numbermaths03.py
@@ -0,0 +1,13 @@
+print(" I will now count my fruits")
+
+print("Apples" ,14 - 10 / 2 % 7 )
+print(" Oranges ", 18 - 9 + 18 / 18 % 7)
+
+print('Now I will count Grapes', 23 + 7 - 9  * 11 / - 4 % 2 - 3.75)
+
+print("It is ", 2 * 18 > 78 / 3 ,"that i have Mangoes also")
+ 
+print("Is 25  greater then 30 ?",25 > 30)
+
+print("Is 388 lesser then 389?", 388<389)
+
