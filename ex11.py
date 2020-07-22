b48f5862695a296594aecfcde0fe875296b98f18
Anands-88 <anandsholla8@gmail.com>
2020-07-22 22:36:33 +0530

    added modified files

---
+++ ex11.py
@@ -0,0 +1,10 @@
+name = input("Enter your name: ")
+
+print("\nPlease answer the following questions.")
+age = input('\nHow old are you? ')
+height = input('\nhow tall are you? ')
+weight = input('\nHow much do you weight? ')
+
+print("\nName   : %r\nAge    : %r\nheight : %r\nweight : %r\n" %(name, age, height, weight))
+
+print(f"\nSo, you're {age} old, {height}foot tall and {weight}kgs heavy.")
