32182c90b85adb29ea489d4215b5ee738928fa46
Anands-88 <anandsholla8@gmail.com>
2020-09-11 21:55:52 +0530

    Modified and added

--- ex40.py
+++ ex40.py
@@ -7,14 +7,13 @@ class Song(object):
         for line in self.lyrics:
             print(line)
 
-happy_bday = """Happy birthday to you
-            I don't want to get sued
-            So I'll stop right there"""
+happy_bday = Song(["Happy birthday to you",
+                "I don't want to get sued",
+                "So I'll stop right there"])
 
+happy_bday.sing_me_a_song()
 
 bulls_on_parade = Song(["They rally around tha family",
                         "With pockets full of shells."])
-
-
 
 bulls_on_parade.sing_me_a_song()
